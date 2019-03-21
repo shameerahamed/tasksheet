0a59dc77fc45677f309cb08f3192ffc5331cadef
Shameer Ahamed <f.shameer.ahamed@accenture.com>
2019-03-21 18:23:33 +0800

    Updated pom.xml

--- src/main/java/com/task/persistence/BaseDao.java
+++ src/main/java/com/task/persistence/BaseDao.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import java.util.Properties;
 
 public class BaseDao {
 	private static BaseDao instance = new BaseDao();
@@ -16,11 +17,20 @@ public class BaseDao {
 	
 	static {
 		try {
+
+			Properties properties = new Properties();
+			System.out.println(System.getenv("JAWSDB_URL"));
+			properties.setProperty("jdbcURL", System.getenv("JAWSDB_URL"));
+			properties.setProperty("username", System.getenv("JAWS_user"));
+			properties.setProperty("password", System.getenv("JAWS_password"));
+
+			log.debug("properties " + properties);
+
 			log.debug("Attempting to Initalise sql Map");
 			String resource = "SqlMapConfig.xml";
 			Reader reader = Resources.getResourceAsReader(resource);
-			sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
-			 log.debug("Initialized SqlMap");
+			sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader, properties);
+			log.debug("Initialized SqlMap");
 		} catch(Exception e) {
 			 log.error("Error intializing BaseIbatisDao ", e);
 	            e.printStackTrace();

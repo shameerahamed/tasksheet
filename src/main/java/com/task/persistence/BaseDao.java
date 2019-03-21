3f71ccaf870824f5cc4a874406bea7d1dd1647f0
Shameer Ahamed <f.shameer.ahamed@accenture.com>
2019-03-21 18:49:15 +0800

    Updated connection details

--- src/main/java/com/task/persistence/BaseDao.java
+++ src/main/java/com/task/persistence/BaseDao.java
@@ -17,19 +17,10 @@ public class BaseDao {
 	
 	static {
 		try {
-
-			Properties properties = new Properties();
-			System.out.println(System.getenv("JAWSDB_URL"));
-			properties.setProperty("jdbcURL", System.getenv("JAWSDB_URL"));
-			properties.setProperty("username", System.getenv("JAWS_user"));
-			properties.setProperty("password", System.getenv("JAWS_password"));
-
-			log.debug("properties " + properties);
-
 			log.debug("Attempting to Initalise sql Map");
 			String resource = "SqlMapConfig.xml";
 			Reader reader = Resources.getResourceAsReader(resource);
-			sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader, properties);
+			sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
 			log.debug("Initialized SqlMap");
 		} catch(Exception e) {
 			 log.error("Error intializing BaseIbatisDao ", e);
